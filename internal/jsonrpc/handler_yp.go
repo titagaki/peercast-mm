d6108f1be484c5d973a262b4269aeeb9433a7e38
titagaki <megane.wo.gurun@gmail.com>
2026-04-01 13:43:14 +0900

    Add channel count to Yellow Pages entries in getYellowPages function

--- internal/jsonrpc/handler_yp.go
+++ internal/jsonrpc/handler_yp.go
@@ -7,9 +7,18 @@ type ypEntry struct {
 	Name         string `json:"name"`
 	URI          string `json:"uri"`
 	AnnounceURI  string `json:"announceUri"`
+	ChannelCount int    `json:"channelCount"`
 }
 
 func (s *Server) getYellowPages() (interface{}, *rpcError) {
+	channelCount := 0
+	if s.ypClient != nil {
+		for _, ch := range s.mgr.List() {
+			if ch.IsBroadcasting() {
+				channelCount++
+			}
+		}
+	}
 	result := make([]ypEntry, len(s.cfg.YPs))
 	for i, entry := range s.cfg.YPs {
 		uri := entry.Addr
@@ -21,6 +30,7 @@ func (s *Server) getYellowPages() (interface{}, *rpcError) {
 			Name:         entry.Name,
 			URI:          uri,
 			AnnounceURI:  uri,
+			ChannelCount: channelCount,
 		}
 	}
 	return result, nil
